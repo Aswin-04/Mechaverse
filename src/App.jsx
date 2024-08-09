20eb6ee943931ff233ff8fa363fcc80dd515dba5
Aswin-04 <e.aswin004@gmail.com>
2024-08-09 17:17:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ParticlesBg from './components/Particles'
+
+function App() {
+
+  return (
+    <>
+     <ParticlesBg></ParticlesBg>
+    </>
+  )
+}
+
+export default App
