a9d186cdbbec0b84c459756990f839a0fb38615f
Aswin-04 <e.aswin004@gmail.com>
2024-08-12 12:02:47 +0530

    Initial project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,68 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: ['12px', '16px'],
+      sm: ['14px', '20px'],
+      base: ['16px', '19.5px'],
+      lg: ['18px', '21.94px'],
+      xl: ['20px', '24.38px'],
+      '2xl': ['24px', '29.26px'],
+      '3xl': ['28px', '50px'],
+      '4xl': ['48px', '58px'],
+      '8xl': ['96px', '106px']
+    },
+    extend: {
+      fontFamily: {
+        palanquin: ['Palanquin', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+        sans: ["var(--font-sora)", ...fontFamily.sans],
+        code: ["var(--font-code)", 'sans-serif'],
+        grotesk: ["var(--font-grotesk)", 'sans-serif']
+      },
+      colors: {
+        'primary': "#90e7e9",
+        "secondary": "#1d3392",
+        "accent": "#5341d9",
+        "tc": "#F5F6FF",
+        "white-400": "rgba(255, 255, 255, 0.80)"
+      },
+      boxShadow: {
+        '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
+      },
+      backgroundImage: {
+        'hero': "url('assets/images/collection-background.svg')",
+        'card': "url('assets/images/thumbnail-background.svg')",
+      },
+      screens: {
+        "wide": "1440px"
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, addComponents, addUtilities }) {
+      addBase({});
+      addComponents({
+        ".container": {
+          "@apply max-w-[77.5rem] mx-auto px-5 md:px-10 lg:px-15 xl:max-w-[87.5rem]":
+            {},
+        },
+        ".h1": {
+          "@apply font-semibold text-[2.5rem] leading-[3.25rem] md:text-[2.75rem] md:leading-[3.75rem] lg:text-[3.25rem] lg:leading-[4.0625rem] xl:text-[3.75rem] xl:leading-[4.5rem]":
+            {},
+        },
+        ".h2": {
+          "@apply text-[1.75rem] leading-[2.5rem] md:text-[2rem] md:leading-[2.5rem] lg:text-[2.5rem] lg:leading-[3.5rem] xl:text-[3rem] xl:leading-tight":
+            {},
+        },
+        ".h3": {
+          "@apply text-[2rem] leading-normal md:text-[2.5rem]": {},
+        },
+        ".button": {
+          "@apply font-code text-xs font-bold uppercase tracking-wider": {},
+        },
+      });
+      addUtilities({});
+    }),
+  ],
 }
-
