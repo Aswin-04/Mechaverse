478759eafea5c14df8efeb5eb797101c8c1a2da9
Aswin-04 <e.aswin004@gmail.com>
2024-08-19 20:14:52 +0530

    working fine v-1

--- src/pages/EventDetails/EventDetails3.jsx
+++ src/pages/EventDetails/EventDetails3.jsx
@@ -6,7 +6,7 @@ const EventDetails3 = () => {
   return (
     <main className="relative">
       <NavBar></NavBar>
-      <section className="max-container mx-auto">
+      <section className="max-container mx-auto text-white">
         <div className="flex justify-center items-center md:h-screen padding-x max-md:pt-[6.688rem]">
           <div className="mt-12 flex flex-col md:flex-row md:h-[calc(100vh-20rem)] overflow-hidden bg-card text-hc pl-6 max-md:px-4 max-md:pb-8">
             <div className="md:flex-shrink-0 md:w-1/3 md:min-w-[300px] md:h-full flex items-center justify-center p-4 bg-card max-md:max-w-[400px] max-md:self-center max-md:mt-8 max-xs:mt-4">
@@ -28,13 +28,13 @@ const EventDetails3 = () => {
 
               <div className="bg-gray-800 p-4 rounded-lg shadow-sm mb-5">
                 <h3 className="h3-text">Guidelines</h3>
-                <ul className="list-disc list-inside pl-6">
+                <ol className="list-decimal list-inside pl-4">
                   {brainTeaseEvent.rules.map((rule, index) => (
                     <li className="mb-2" key={index} style={{ listStylePosition: "outside" }}>
                       {rule}
                     </li>
                   ))}
-                </ul>
+                </ol>
               </div>
 
               <div className="bg-gray-800 p-4 rounded-lg shadow-sm mb-5">
@@ -52,7 +52,7 @@ const EventDetails3 = () => {
                 <div className="flex gap-5">
                   {brainTeaseEvent.coordinators.map((coordinator, index) => (
                     <div className="flex flex-col items-center" key={index}>
-                      <div className="w-[120px] h-[120px] rounded-full overflow-hidden shadow-lg mt-4 mb-4 max-xxs:w-[100px] max-xxs:h-[100px]">
+                      <div className="w-[120px] h-[120px] rounded-full overflow-hidden shadow-lg mt-4 mb-4 max-xxs:w-[85px] max-xxs:h-[85px]">
                         <img
                           src={coordinator.img}
                           alt={coordinator.name}
