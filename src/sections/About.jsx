c9f699c6dad250bbe97180fce4e539b90e2786ba
Aswin-04 <e.aswin004@gmail.com>
2024-08-27 22:10:06 +0530

    add animation to events and aboutus section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -6,25 +6,26 @@ const About = () => {
   return (
     <section className="max-container ">
       {/* for small screens */}
-      <div className="flex flex-col gap-8 lg:hidden">
+      <div data-aos="fade-down" className="flex flex-col gap-8 lg:hidden">
         <div>
-          <h2 className="h3 text-center uppercase font-palanquin font-bold tracking-tight text-transparent bg-clip-text bg-radial-primary-secondary">
+          <h2 ata-aos="fade-in" data-aos-delay="250" className="h3 text-center uppercase font-palanquin font-bold tracking-tight text-transparent bg-clip-text bg-radial-primary-secondary">
             {aboutContent.title}
           </h2>
         </div>
         <div className="flex flex-col items-center  px-4 py-8 rounded-xl  bg-[#020809]/50 border-[1px] border-white/30">
           <div className="">
             <img
+              data-aos="zoom-in"
               src={aboutContent.img}
               alt={aboutContent.imgAlt}
               width={aboutContent.imgWidth}
               className="rounded-3xl"
             />
           </div>
-          <p className="font-montserrat leading-8 text-hc max-lg:max-w-xl max-md:max-w-md mt-8">
+          <p data-aos="fade-down" className="font-montserrat leading-8 text-hc max-lg:max-w-xl max-md:max-w-md mt-8">
             {aboutContent.description}
           </p>
-          <div className="inline-flex max-w-sm mx-auto mt-6">
+          <div data-aos="zoom-in" className="inline-flex max-w-sm mx-auto mt-6">
             <Link
               to={aboutContent.linkTo}
               className="w-full flex justify-center items-center whitespace-nowrap transition duration-150 ease-in-out font-medium rounded px-4 py-1.5 text-zinc-950 bg-gradient-to-r from-tc/80 via-tc to-tc/80 hover:bg-hc group"
@@ -37,9 +38,10 @@ const About = () => {
       </div>
 
       {/* for large screens */}
-      <div className="flex max-lg:hidden rounded-xl  bg-[#020809]/50 border-[1px] border-white/30">
+      <div data-aos="fade-down" className="flex max-lg:hidden rounded-xl  bg-[#020809]/50 border-[1px] border-white/30">
         <div className="min-w-[45%] pl-8 flex justify-center items-center">
           <img
+            data-aos="zoom-in"
             src={aboutContent.img}
             alt={aboutContent.imgAlt}
             width={aboutContent.imgWidth}
@@ -47,13 +49,13 @@ const About = () => {
           />
         </div>
         <div className="flex flex-col items-center  px-8 py-8">
-          <h2 className="h3 text-center uppercase font-palanquin font-bold tracking-tight text-transparent bg-clip-text bg-radial-primary-secondary">
+          <h2 data-aos="fade-in" data-aos-delay="250" className="h3 text-center uppercase font-palanquin font-bold tracking-tight text-transparent bg-clip-text bg-radial-primary-secondary">
             {aboutContent.title}
           </h2>
-          <p className="font-montserrat leading-8 text-hc max-w-4xl mt-6">
+          <p data-aos="fade-down" className="font-montserrat leading-8 text-hc max-w-4xl mt-6">
             {aboutContent.description}
           </p>
-          <div className="inline-flex max-w-sm mx-auto mt-10">
+          <div data-aos="zoom-in" className="inline-flex max-w-sm mx-auto mt-10">
             <Link
               to={"/about"}
               className="w-full flex justify-center items-center whitespace-nowrap transition duration-150 ease-in-out font-medium rounded px-4 py-1.5 text-zinc-950 bg-gradient-to-r from-tc/80 via-tc to-tc/80 hover:bg-hc group"
--- src/sections/Events.jsx
+++ src/sections/Events.jsx
@@ -16,17 +16,30 @@ const Events = () => {
   return (
     <section className="max-container ">
       {/* for small screens */}
-      <div className="flex flex-col gap-8 lg:hidden">
+      <div data-aos="fade-down" className="flex flex-col gap-8 lg:hidden">
         <div>
-          <h2 className="h3 text-center uppercase font-palanquin font-bold tracking-tight text-transparent bg-clip-text bg-radial-primary-secondary">{eventsContent.title}</h2>
+          <h2 data-aos="fade-in" data-aos-delay="250" className="h3 text-center uppercase font-palanquin font-bold tracking-tight text-transparent bg-clip-text bg-radial-primary-secondary">
+            {eventsContent.title}
+          </h2>
         </div>
         <div className="flex flex-col items-center px-4 py-8 rounded-xl bg-[#020809]/50 border-[1px] border-white/30">
           <div>
-            <img src={eventsContent.img} alt={eventsContent.imgAlt} width={eventsContent.imgWidth} className="rounded-3xl" />
+            <img
+              data-aos="zoom-in"
+              src={eventsContent.img}
+              alt={eventsContent.imgAlt}
+              width={eventsContent.imgWidth}
+              className="rounded-3xl"
+            />
           </div>
-          <p className="font-montserrat leading-8 text-hc max-lg:max-w-lg max-md:max-w-md mt-8">{eventsContent.description}</p>
-          <div className="inline-flex max-w-sm mx-auto mt-6">
-            <button onClick={handleExploreClick} className="w-full flex justify-center items-center whitespace-nowrap transition duration-150 ease-in-out font-medium rounded px-4 py-1.5 text-zinc-950 bg-gradient-to-r from-tc/80 via-tc to-tc/80 hover:bg-hc group">
+          <p data-aos="fade-down" className="font-montserrat leading-8 text-hc max-lg:max-w-lg max-md:max-w-md mt-8">
+            {eventsContent.description}
+          </p>
+          <div data-aos="zoom-in" className="inline-flex max-w-sm mx-auto mt-6">
+            <button
+              onClick={handleExploreClick}
+              className="w-full flex justify-center items-center whitespace-nowrap transition duration-150 ease-in-out font-medium rounded px-4 py-1.5 text-zinc-950 bg-gradient-to-r from-tc/80 via-tc to-tc/80 hover:bg-hc group"
+            >
               {eventsContent.linkLabel}{" "}
               <ArrowRight className="w-3 h-3 tracking-normal text-[#003f6c]-500 group-hover:translate-x-1 transition-transform duration-150 ease-in-out ml-1" />
             </button>
@@ -35,15 +48,28 @@ const Events = () => {
       </div>
 
       {/* for large screens */}
-      <div className="flex max-lg:hidden rounded-xl bg-[#020809]/50 border-[1px] border-white/30 py-4">
+      <div data-aos="fade-down" className="flex max-lg:hidden rounded-xl bg-[#020809]/50 border-[1px] border-white/30 py-4">
         <div className="min-w-[45%] pl-8 flex justify-center items-center">
-          <img src={eventsContent.img} alt={eventsContent.imgAlt} width={eventsContent.imgWidth} className="rounded-3xl" />
+          <img
+            data-aos="zoom-in"
+            src={eventsContent.img}
+            alt={eventsContent.imgAlt}
+            width={eventsContent.imgWidth}
+            className="rounded-3xl"
+          />
         </div>
         <div className="flex flex-col items-center px-8 py-8">
-          <h2 className="h3 text-center uppercase font-palanquin font-bold tracking-tight text-transparent bg-clip-text bg-radial-primary-secondary">{eventsContent.title}</h2>
-          <p className="font-montserrat leading-8 text-hc max-w-4xl mt-6">{eventsContent.description}</p>
-          <div className="inline-flex max-w-sm mx-auto mt-10">
-            <button onClick={handleExploreClick} className="w-full flex justify-center items-center whitespace-nowrap transition duration-150 ease-in-out font-medium rounded px-4 py-1.5 text-zinc-950 bg-gradient-to-r from-tc/80 via-tc to-tc/80 hover:bg-hc group">
+          <h2 data-aos="fade-in" data-aos-delay="250" className="h3 text-center uppercase font-palanquin font-bold tracking-tight text-transparent bg-clip-text bg-radial-primary-secondary">
+            {eventsContent.title}
+          </h2>
+          <p data-aos="fade-down" className="font-montserrat leading-8 text-hc max-w-4xl mt-6">
+            {eventsContent.description}
+          </p>
+          <div data-aos="zoom-in" className="inline-flex max-w-sm mx-auto mt-10">
+            <button
+              onClick={handleExploreClick}
+              className="w-full flex justify-center items-center whitespace-nowrap transition duration-150 ease-in-out font-medium rounded px-4 py-1.5 text-zinc-950 bg-gradient-to-r from-tc/80 via-tc to-tc/80 hover:bg-hc group"
+            >
               {eventsContent.linkLabel}{" "}
               <ArrowRight className="w-3 h-3 tracking-normal text-[#003f6c]-500 group-hover:translate-x-1 transition-transform duration-150 ease-in-out ml-1" />
             </button>
