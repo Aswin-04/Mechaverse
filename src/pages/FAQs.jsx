bf1849bc7f06a12df6abcf1cb2a29be832cf5083
Aswin-04 <e.aswin004@gmail.com>
2024-08-31 21:45:49 +0530

    add animation to FAQs page

--- src/pages/FAQs.jsx
+++ src/pages/FAQs.jsx
@@ -1,11 +1,22 @@
+import { useEffect, useState } from "react"
 import Accordion from "../components/Accordion";
 import NavBar from "../components/NavBar";
 import GradientHeader from "../components/GradientHeader";
 import { Footer } from "../sections";
 import { faqContent } from "../constants";
-import { useState } from "react";
+import AOS from "aos"
+import "aos/dist/aos.css"
 
 const FAQs = () => {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      easing: "ease-in-out-cubic",
+      once: true,
+    })
+  })
+
   const [openIndex, setOpenIndex] = useState(null);
   const handleToggle = (index) => {
     setOpenIndex((currentIndex) => (currentIndex === index ? null : index));
@@ -14,11 +25,11 @@ const FAQs = () => {
     <section className="relative">
       <NavBar />
       <section className="padding-x h-screen-minus max-container mt-[6.688rem] flex flex-col items-center">
-        <div className="flex justify-center items-center p-12">
+        <div data-aos="fade-down" className="flex justify-center items-center p-12">
           <GradientHeader label="FAQs" className="font-sans" />
         </div>
         <div className="grow w-full">
-          <ul className=" bg-gray-800 flex-col flex gap-[1px]">
+          <ul data-aos="fade-down" className=" bg-gray-800 flex-col flex gap-[1px]">
             {faqContent.map((content, index) => (
               <Accordion
                 content={content}
